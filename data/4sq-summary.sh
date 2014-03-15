08ceb903e8bd6d36f4fb3aefa2c049903838f6b3
Nattawut Phetmak <neizod@gmail.com>
2014-03-16 06:57:03 +0700

    fix broken interfaces

--- data/4sq-summary.sh
+++ data/4sq-summary.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-oldest=$(ls -1 raw-data/ | head -n 1)
-newest=$(ls -1 -r raw-data/ | head -n 1)
+oldest=$(ls -1 raw-checkins/ | head -n 1)
+newest=$(ls -1 -r raw-checkins/ | head -n 1)
 
 paste raw-checkins/{$oldest,$newest} |
     awk 'BEGIN{OFS="\t"} {print "- "$1, $4-$2}' > RESTAURANTS.md
